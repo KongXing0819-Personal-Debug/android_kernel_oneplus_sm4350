1005da32b65ac41eba4a0d90d6f1b67566cb3bbc
KongXing0819 <1902002294@qq.com>
2025-10-12 17:56:28 +0800

    Add Workflows

--- fs/open.c
+++ fs/open.c
@@ -445,10 +445,10 @@ out:
 }
 
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
+{
 #if defined(CONFIG_KSU) && defined(CONFIG_KSU_TRACEPOINT_HOOK)
 	trace_ksu_trace_faccessat_hook(&dfd, &filename, &mode, NULL);
 #endif
-{
 	return do_faccessat(dfd, filename, mode);
 }
 
