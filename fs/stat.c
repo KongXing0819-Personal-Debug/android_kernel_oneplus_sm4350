5e3293819c9f10cfd5587681349d5d2a6b721987
KongXing0819 <b170420nc@gmail.com>
2025-11-11 22:49:35 +0800

    Delete Tracepoint hook code,because it is no useful for newer Sukisu Ultra.

--- fs/stat.c
+++ fs/stat.c
@@ -21,10 +21,6 @@
 #include <linux/uaccess.h>
 #include <asm/unistd.h>
 
-#if defined(CONFIG_KSU) && defined(CONFIG_KSU_TRACEPOINT_HOOK)
-#include <../drivers/kernelsu/ksu_trace.h>
-#endif
-
 /**
  * generic_fillattr - Fill in the basic attributes from the inode struct
  * @inode: Inode to use as the source
@@ -377,9 +373,6 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 #if defined(CONFIG_KSU) && defined(CONFIG_KSU_MANUAL_HOOK)
 	ksu_handle_stat(&dfd, &filename, &flag);
 #endif
-#if defined(CONFIG_KSU) && defined(CONFIG_KSU_TRACEPOINT_HOOK)
-	trace_ksu_trace_stat_hook(&dfd, &filename, &flag);
-#endif
 
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
@@ -534,9 +527,6 @@ SYSCALL_DEFINE4(fstatat64, int, dfd, const char __user *, filename,
 	
 #if defined(CONFIG_KSU) && defined(CONFIG_KSU_MANUAL_HOOK) // 32-bit su
 	ksu_handle_stat(&dfd, &filename, &flag);
-#endif
-#if defined(CONFIG_KSU) && defined(CONFIG_KSU_TRACEPOINT_HOOK)
-	trace_ksu_trace_stat_hook(&dfd, &filename, &flag); /* 32-bit su support */
 #endif
 
 	error = vfs_fstatat(dfd, filename, &stat, flag);
